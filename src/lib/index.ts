1b11910001b7034fe0c319abba0929f3ff9d1178
kashan-ahmad <kashanahmed@hwryk.com>
2023-04-09 13:13:49 +0500

    feat: added strings

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
 export * from './utils'
+export * from './strings'
 
 export { default as SassyError } from './SassyError'
---
+++ src/lib/strings.ts
@@ -0,0 +1,3 @@
+export const DEFAULT_ERROR_MESSAGE = 'Something went wrong',
+  DEFAULT_SUCCESS_MESSAGE = 'All changes saved',
+  DEFAULT_LOADING_MESSAGE = 'Loading'
