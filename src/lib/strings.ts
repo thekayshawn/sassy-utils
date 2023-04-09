2ae8d289b65bb66a161f12365e20873ea30128d0
kashan-ahmad <kashanahmed@hwryk.com>
2023-04-09 13:27:13 +0500

    feat: exported strings as an object

--- src/lib/strings.ts
+++ src/lib/strings.ts
@@ -1,3 +1,5 @@
-export const DEFAULT_ERROR_MESSAGE = 'Something went wrong',
-  DEFAULT_SUCCESS_MESSAGE = 'All changes saved',
-  DEFAULT_LOADING_MESSAGE = 'Loading'
+export const strings = {
+  DEFAULT_LOADING_MESSAGE: 'Loading',
+  DEFAULT_SUCCESS_MESSAGE: 'All changes saved',
+  DEFAULT_ERROR_MESSAGE: 'Something went wrong',
+}
